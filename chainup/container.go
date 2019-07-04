5293e8a02eb651a2583e66cebc4e25cb3b3c09af
Andrej Bencic <bencicandrej94@gmail.com>
2019-07-04 11:11:48 +0200

    Added revive linter to project

--- chainup/container.go
+++ chainup/container.go
@@ -1,9 +1,12 @@
 package chainup
 
+// Container is an abstraction of a runnable
+// that needs to be executed on a target host machine.
 type Container struct {
 	Name string
 }
 
+// NewContainer creates a new instance of a Container struct.
 func NewContainer(name string) *Container {
 	return &Container{Name: name}
 }
